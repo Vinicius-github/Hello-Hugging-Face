8db496b504fbb569f80a751c11f07fccd6277b81
Vinícius <146575176+Vinicius-github@users.noreply.github.com>
2024-03-17 15:59:25 -0300

    Delete Mask Filling directory
    Delete to standardize the folder names.

--- Mask Filling/mask_fill.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-#Preenchimento de espaçõs em branco com um texto informado
-unmasker = pipeline("fill-mask")
-res = unmasker("This course will teach you all about <mask> models.", top_k=2)
-print(res)
